045673dbc9d995cceeecb059fc9c059af242dc49
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-23 02:19:55 +0600

    database connection established

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 9000;
 
@@ -9,22 +10,37 @@ const port = process.env.PORT || 9000;
 app.use(cors());
 // parse application/json
 app.use(bodyParser.json());
+// env using
+require("dotenv").config();
 
 //  mongodb Connection Code
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.isfsk8s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("mongoDb Connected");
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const volunteerCollection = client
+      .db("volunteerDb")
+      .collection("volunteerData");
+
+    // getting data
+    app.get("/db", async (req, res) => {
+      const query = {};
+      const cursor = volunteerCollection.find(query);
+      const volunteers = await cursor.toArray();
+      res.send(volunteers);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 // server
 
