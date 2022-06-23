e64909bef9486e5f1075c8b3bd39e94941fa1b38
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-23 08:01:23 +0600

    delete from db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 9000;
 
@@ -28,12 +28,35 @@ async function run() {
       .db("volunteerDb")
       .collection("volunteerData");
 
-    // getting data
+    // getting all data
     app.get("/db", async (req, res) => {
       const query = {};
       const cursor = volunteerCollection.find(query);
       const volunteers = await cursor.toArray();
       res.send(volunteers);
+    });
+
+    // getting single data
+    app.get("/db/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const volunteer = await volunteerCollection.findOne(query);
+      res.send(volunteer);
+    });
+
+    // post data
+    app.post("/db", async (req, res) => {
+      const newVolunteer = req.body;
+      const result = await volunteerCollection.insertOne(newVolunteer);
+      res.send(result);
+    });
+
+    // DELETE a document
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const restResult = await volunteerCollection.deleteOne(query);
+      res.send(restResult);
     });
   } finally {
     // await client.close();
